0b1a2b2a2948931bb44336990fc3f20e56b9a0da
gsikaras <sykarasioannis@hotmail.com>
2023-10-05 15:41:29 +0300

    delopin pin pin

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,47 +1,88 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {
-  // instance properties
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+// const Person = function (firstName, birthYear) {
+//   // instance properties
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
 
-  //Never do this, no methods into constructor function!
-  //   this.calcAge = function () {
-  //     console.log(2037 - this.birthYear);
-  //   };
-};
+//   //Never do this, no methods into constructor function!
+//   //   this.calcAge = function () {
+//   //     console.log(2037 - this.birthYear);
+//   //   };
+// };
 
-const jonas = new Person('Jonas', 1991);
-console.log(jonas);
+// const jonas = new Person('Jonas', 1991);
+// console.log(jonas);
 
-// 1. New {} is created
-// 2. function called , this = new emppty object {}
-// 3. the new obeject {} is linked to prototype
-// 4. function automatically return {}
+// // 1. New {} is created
+// // 2. function called , this = new emppty object {}
+// // 3. the new obeject {} is linked to prototype
+// // 4. function automatically return {}
 
-const matilda = new Person('Matilda', 2017);
-const jack = new Person('Jack', 1975);
-console.log(matilda, jack);
+// const matilda = new Person('Matilda', 2017);
+// const jack = new Person('Jack', 1975);
+// console.log(matilda, jack);
 
-console.log(jonas instanceof Person);
+// console.log(jonas instanceof Person);
 
-//prototypes
-console.log(Person.prototype);
+// //prototypes
+// console.log(Person.prototype);
 
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
-};
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
 
-jonas.calcAge();
-matilda.calcAge();
+// jonas.calcAge();
+// matilda.calcAge();
 
-Person.prototype.species = 'Homo Sapiens';
-console.log(jonas, matilda);
+// Person.prototype.species = 'Homo Sapiens';
+// console.log(jonas, matilda);
 
-console.log(jonas.hasOwnProperty('firstName'));
-console.log(jonas.hasOwnProperty('species'));
+// console.log(jonas.hasOwnProperty('firstName'));
+// console.log(jonas.hasOwnProperty('species'));
 
-console.log(jonas.__proto__);
-// onject.prototype (top of the prototype chain)
-console.log(jonas.__proto__.__proto__);
-console.log(jonas.__proto__.__proto__.__proto__);
+// console.log(jonas.__proto__);
+// // onject.prototype (top of the prototype chain)
+// console.log(jonas.__proto__.__proto__);
+// console.log(jonas.__proto__.__proto__.__proto__);
+
+// console.dir(Person.prototype.constructor);
+
+// const arr = [3, 6, 6, 5, 6, 9, 9];
+
+// console.log(arr.__proto__);
+
+// console.log(arr.__proto__ === Array.prototype);
+
+// console.log(arr.__proto__.__proto__);
+
+// Array.prototype.unique = function () {
+//   return [...new Set(this)];
+// };
+
+// console.log(arr.unique());
+
+// const h1 = document.querySelector('h1');
+
+// console.dir(x => x + 1);
+
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+
+// Car.prototype.accelerate = function () {
+//   console.log((this.speed += 10));
+// };
+
+// Car.prototype.brake = function () {
+//   console.log((this.speed -= 5));
+// };
+
+// const BMW = new Car('BMW', 120);
+// const Mercedes = new Car('Mercedes', 95);
+
+// BMW.accelerate();
+// BMW.accelerate();
+// BMW.brake();
+// BMW.accelerate();
