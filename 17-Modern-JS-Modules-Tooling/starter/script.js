8f7427519d98d2da9fb60ead722e6b3b46c3f371
gsikaras <sykarasioannis@hotmail.com>
2023-11-29 17:26:37 +0200

    Top level await/module pattern

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -3,12 +3,41 @@
 // addToCart('bread', 5);
 // console.log(price, tq);
 
-console.log('Importing module');
-// console.log(shippingCost);
+// console.log('Importing module');
+// // console.log(shippingCost);
 
-// import * as ShoppingCart from './shoppingCart.js';
-// ShoppingCart.addToCart('bread', 5);
-// console.log(ShoppingCart.totalPrice);
+// // import * as ShoppingCart from './shoppingCart.js';
+// // ShoppingCart.addToCart('bread', 5);
+// // console.log(ShoppingCart.totalPrice);
 
-import add from './shoppingCart.js';
-add('pizza', 2);
+// import add, { addToCart, tq } from './shoppingCart.js';
+// add('pizza', 2);
+// addToCart('oranges', 5);
+// console.log(tq);
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to the cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
